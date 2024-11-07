9fc8be7a751f504ab2ecf9d3d66d465efd85b5b8
PedroHAP2810 <pedroheriquepessoa@gmail.com>
2024-11-06 21:54:51 -0300

    feito em sala

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,66 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import Pagina from '@/components/Pagina'
+import { Button, Card, Col, Row } from 'react-bootstrap'
+
+export default function HomePage() {
+
+
+  const filiais = JSON.parse(localStorage.getItem("filiais")) || []
+  const produtos = JSON.parse(localStorage.getItem("produtos")) || []
+  const funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || []
+  const entregadores = JSON.parse(localStorage.getItem("entregadores")) || []
+  const clientes = JSON.parse(localStorage.getItem("clientes")) || []
+
+  const lista = [
+    {
+      nome: "Filial",
+      imagem: "https://www.sindipublicos.com.br/wp-content/uploads/2022/02/drogasil-site.jpg", quantidade: filiais.length,
+      link: "/filial"
+    },
+    {
+      nome: "Produtos",
+      imagem: "https://ictq.com.br/images/Redes_de_farm%C3%A1cias_registram_retra%C3%A7%C3%A3o_de_22_em_abril.jpg", quantidade: produtos.length,
+      link: "/produto"
+    },
+    {
+      nome: "Funcionários",
+      imagem: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia13814/funcao-social-farmacias-farmaceuticos-dicas-cursos-cpt.jpg", quantidade: funcionarios.length,
+      link: "/funcionario"
+    },
+    {
+      nome: "Entregadores",
+      imagem: "https://www.designi.com.br/images/preview/10102082.jpg", quantidade: entregadores.length,
+      link: "/entregador"
+    },
+    {
+      nome: "Clientes",
+      imagem: "https://www.triersistemas.com.br/blog/wp-content/uploads/2020/08/passos-para-fidelizar-cliente-triersistemas.jpg", quantidade: clientes.length,
+      link: "/cliente"
+    },
+  ]
+
+
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Pagina titulo={"Projeto Farmácias"}>
+      <Row md={5}>
+        {lista.map(item => (
+          <Col className='py-2'>
+            <Card style={{height: '100%'}}>
+              <Card.Img src={item.imagem} style={{ height: '100%' }} />
+              <Card.Body>
+                <Card.Title>{item.nome}</Card.Title>
+                Cadastrados: {item.quantidade}
+              </Card.Body>
+              <Card.Footer className='text-end'>
+                <Button href={item.link}>Ver Lista</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </Row>
+    </Pagina>
+  )
 }
---
+++ src/components/Pagina.js
@@ -0,0 +1,35 @@
+'use client'
+
+import { Container, Nav, Navbar } from "react-bootstrap"
+
+
+export default function Pagina({ titulo, children }) {
+
+  return (
+    <>
+    
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Pagina Inicial</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/clientes">Clientes</Nav.Link>
+            <Nav.Link href="/funcionarios">Funcionários</Nav.Link>
+            <Nav.Link href="/entregadores">Entregadores</Nav.Link>
+            <Nav.Link href="/produtos">Produtos</Nav.Link>
+            <Nav.Link href="/filiais">Filiais</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      
+      <div className="bg-secondary text-center text-white py-2">
+        <h1>{titulo}</h1>
+      </div>
+
+      
+      <Container className="mt-2">
+        {children}
+      </Container>
+    </>
+  )
+}
